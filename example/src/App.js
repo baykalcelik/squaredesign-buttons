e6dbed3d9c3e50d31c4b935eb9be10da3e69b48d
baykalcelik <baykalcelik2022@gmail.com>
2022-09-19 22:33:00 +0300

    v1.0.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,86 @@
 import React from 'react'
 
-import { ExampleComponent } from 'squaredesign-buttons'
+import { Button, ExampleComponent } from 'squaredesign-buttons'
 import 'squaredesign-buttons/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+    <ExampleComponent text="İlk library deneme" />
+
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s01"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s03"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s05"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s08"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s12"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s15"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s18"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s20"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s25"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s30"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+
+    <br/>
+
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "plarge" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "pxlarge" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+
+
+    <br/> <br/>
+    <Button text="Hesapla" type="primary" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="primaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="primaryLink" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+
+
+
+    <br/> <br/>
+    <Button text="Hesapla" type="secondary" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryLink" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+
+    <br/> <br/>
+    <Button text="Hesapla" type="success" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="successOutlined" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="successLink" size="s10"  disabledstyle ={false} border = "rounded-5" padding = "psmall" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+
+    <br/> <br/>
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "pill" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "circle" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-2" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-3" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-4" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-6" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-7" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-8" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-9" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-10" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    <Button text="Hesapla" type="secondaryOutlined" size="s10"  disabledstyle ={false} border = "rounded-20" padding = "pmedium" handleClick={(e)=>{alert("Buton üzerindeki yazı :", e.target.innerText)}} />
+    </> 
+  )
 }
 
 export default App
